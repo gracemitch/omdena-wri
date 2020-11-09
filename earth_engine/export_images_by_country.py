8d2a5d40cbfd30a04455a47b2928df283424c0b2
gracemitchw <grace@watttime.org>
2020-11-09 18:58:44 -0500

    only download missing

--- earth_engine/export_images_by_country.py
+++ earth_engine/export_images_by_country.py
@@ -2,10 +2,12 @@ import argparse
 import ee
 import json
 import os
+import numpy as np
 import pandas as pd
 import time
 from tqdm import tqdm
 import urllib as ur
+from google.cloud import storage
 from google.auth.transport.requests import AuthorizedSession
 from google.oauth2 import service_account
 
@@ -13,6 +15,8 @@ PROJECT = 'omdena-wri'
 SERVICE_ACCOUNT_STR ='sa1-311@omdena-wri.iam.gserviceaccount.com'
 KEY = 'private-key.json'
 BUCKET = '1182020'
+COLLECTION = 'MODIS'
+
 
 def authenticate(project, service_account_str, key):
     # # GCP instructions
@@ -34,6 +38,7 @@ def authenticate(project, service_account_str, key):
     ee.Initialize()
     return session
 
+
 def get_platform_countries():
     areas_by_platform = pd.read_csv('./platforms/areas_served_by_platform.csv')
     country_codes = pd.read_csv('./platforms/country_codes.csv').rename(columns={'country': 'country_clean'})
@@ -43,6 +48,7 @@ def get_platform_countries():
     countries_dict = dict(zip(countries['alpha3code'], countries['country']))
     return countries_dict
 
+
 def get_country_geojson(country_alpha3):
     # create geojson file from url
     url = F'https://raw.githubusercontent.com/johan/world.geo.json/master/countries/{country_alpha3}.geo.json'
@@ -50,11 +56,13 @@ def get_country_geojson(country_alpha3):
         data = json.loads(response.read().decode())
         return data
 
+
 def country_poly(country_alpha3):
     geojson = get_country_geojson(country_alpha3)
     coords = geojson['features'][0]['geometry']['coordinates']
     poly = ee.Geometry.Polygon(coords)
     return poly, coords
+
 
 def get_date_ranges(asset_id):
     if asset_id == 'MODIS/006/MOD11A1':
@@ -97,10 +105,11 @@ def get_image_ids(session, asset_id, country_alpha3, coords):
         image_ids_list = list(map(lambda x: x['id'], content_json['images']))
         image_ids_sublists.append(image_ids_list)
         # avoid too many calls too fast
-        time.sleep(5)
+        time.sleep(1)
 
     image_ids_flat = [item for sublist in image_ids_sublists for item in sublist]
     return image_ids_flat
+
 
 def get_image_metadata(collection_str, image_id, country_alpha3, poly):
     if collection_str == 'MODIS':
@@ -120,6 +129,13 @@ def get_image_metadata(collection_str, image_id, country_alpha3, poly):
     return df
 
 
+def get_file_names(bucket_str, path, file_extension):
+    client = storage.Client()
+    bucket = client.bucket(bucket_str)
+    blobs = list(bucket.list_blobs(prefix=path))
+    return [blob.name for blob in blobs if blob.name.endswith(file_extension)]
+
+
 def export_collection_metadata(session, bucket, collection_str, country_alpha3):
     poly, coords = country_poly(country_alpha3)
 
@@ -136,31 +152,52 @@ def export_collection_metadata(session, bucket, collection_str, country_alpha3):
     collection_metadata = pd.concat(collection_metadata)
     collection_metadata.to_csv(F'gs://{bucket}/earth_engine/metadata/{collection_str}/{country_alpha3}.csv', index=False)
 
+
+def get_missing_images(image_path, metadata, bucket):
+    images_desired = list(map(lambda x: F"{image_path}{x.replace('/', '-')}.tif", metadata['image_id']))
+    images_complete = get_file_names(bucket, image_path, '.tif')
+    images_missing = np.setdiff1d(images_desired, images_complete)
+    print(F'{len(images_missing)}/{len(images_desired)} images remaining...')
+    return list(map(lambda x: F"{x.replace('-', '/').replace('.tif', '')}", images_missing))
+
+
 def export_collection_images(bucket, collection_str, country_alpha3):
     # https://colab.research.google.com/github/csaybar/EEwPython/blob/dev/10_Export.ipynb
 
     poly, coords = country_poly(country_alpha3)
     metadata = pd.read_csv(F'gs://{bucket}/earth_engine/metadata/{collection_str}/{country_alpha3}.csv')
 
-    count = 0
-    for image_id in tqdm(metadata['image_id'].unique(), total=metadata['image_id'].nunique(), desc=F'Exporting {country_alpha3} {collection_str} images... '):
-        if collection_str == 'MODIS':
-            image = ee.Image(image_id).select('LST_Day_1km').clip(poly) 
-            image_fn = image_id.replace('/', '-')
-            task = ee.batch.Export.image.toCloudStorage(**{
-                'image': image,
-                'bucket': bucket,
-                'fileNamePrefix': F'earth_engine/images_tif/{collection_str}/{country_alpha3}/{image_fn}',
-                'region': poly,
-                'fileFormat': 'GeoTIFF',
-                'scale': 1000,
-                'crs': 'EPSG:4326',
-                'maxPixels': 1e10
-            })
-            task.start()
-        # avoid too many calls too fast
-        time.sleep(5)
+    image_path = F'earth_engine/images_tif/{collection_str}/{country_alpha3}/'
+    images_missing = get_missing_images(image_path, metadata, bucket)
 
+    if len(images_missing) > 0:
+        for image_id in tqdm(images_missing, total=len(images_missing), desc=F'Exporting {country_alpha3} {collection_str} images... '):
+            if collection_str == 'MODIS':
+                image = ee.Image(image_id.replace(image_path, '')).select('LST_Day_1km').clip(poly) 
+                image_fn = image_id.replace('/', '-')
+                task = ee.batch.Export.image.toCloudStorage(**{
+                    'image': image,
+                    'bucket': bucket,
+                    'fileNamePrefix': F'earth_engine/images_tif/{collection_str}/{country_alpha3}/{image_fn}',
+                    'region': poly,
+                    'fileFormat': 'GeoTIFF',
+                    'scale': 1000,
+                    'crs': 'EPSG:4326',
+                    'maxPixels': 1e10
+                })
+                task.start()
+            # avoid too many calls too fast
+            time.sleep(1)
+
+
+def get_missing_metadata(countries_dict, bucket, collection_str):
+    metadata_path = F'earth_engine/metadata/{collection_str}/'
+    metadata_desired = list(map(lambda x: F'{metadata_path}{x}.csv', countries_dict.keys()))
+    metadata_complete = get_file_names(bucket, metadata_path, '.csv')
+    metadata_missing = np.setdiff1d(metadata_desired, metadata_complete)
+    print(F'{len(metadata_missing)}/{len(metadata_desired)} metadata files remaining...')
+    # country alpha 3 code
+    return list(map(lambda x: F"{x.replace(metadata_path, '').replace('.csv', '')}", metadata_missing))
 
 
 if __name__ == '__main__':
@@ -170,10 +207,28 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     session = authenticate(PROJECT, SERVICE_ACCOUNT_STR, KEY)
-
     countries_dict = get_platform_countries()
-    for country_alpha3 in countries_dict.keys():
-        if args.download_metadata:
-            export_collection_metadata(session, BUCKET, 'MODIS', country_alpha3)
-        if args.download_images:
-            export_collection_images(BUCKET, 'MODIS', country_alpha3)
+
+    if args.download_metadata:
+        countries = get_missing_metadata(countries_dict, BUCKET, COLLECTION)
+        metadata_logs = []
+        for country_alpha3 in countries:
+            try:
+                export_collection_metadata(session, BUCKET, COLLECTION, country_alpha3)
+                status = 'SUCCESS'
+                e = None
+            except Exception as e:
+                status = 'FAIL'
+                continue
+            metadata_log = pd.DataFrame({
+                'country_alpha3': [country_alpha3], 
+                'status': [status], 
+                'exception': [e],
+            })
+            metadata_logs.append(metadata_log)
+        metadata_log = pd.concat(metadata_logs)
+        metadata_log.to_csv(F'{COLLECTION}_metadata_log.csv', index=False)
+    
+    if args.download_images:
+        for country_alpha3 in countries_dict.keys():
+            export_collection_images(BUCKET, COLLECTION, country_alpha3)
